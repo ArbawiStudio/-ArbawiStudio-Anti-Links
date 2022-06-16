f747f4da8b260864e423d460c6a3b1380a873167
TOBZi <81484769+ArbawiStudio@users.noreply.github.com>
2022-06-16 17:40:04 +0200

    Developed by OokamiCodes
    Upgraded by TOBZi

---
+++ index.js
@@ -0,0 +1,23 @@
+require('discord.js')
+
+module.exports = async(client, message, options) => {
+    let Links = [ 'www', '.com', '.xyz', '.io', 'facebook', 'fb', 'youtube', 'yt', 'stackoverflow', 'discord.js.org', 'https://', 'http://', '.net', '.gg', '.org', 'ww', 'download' ]
+    let FoundLink = false;
+    let Message = (options && options.WarnMessage) || `<@${message.author.id}> **The Links is not Allowed Here**`;
+    let StaffRole = (options && options.StaffRole)
+
+    for (var i in Links) {
+        if(message.content.toLowerCase().includes(Links[i].toLowerCase())) FoundLink = true;
+    }
+
+    for (var i in StaffRole) {
+        if(!message.member.permissions.has('ADMINISTRATOR') && !message.member.permissions.has('MANAGE_GUILD')) FoundLink = false;
+        if(message.member.roles.cache.has(StaffRole)) FoundLink = false;
+    }
+
+    if(FoundLink) {
+        if(message.member.permissions.has('ADMINISTRATOR') && !message.member.permissions.has('MANAGE_GUILD')) return;
+        message.delete()
+        message.channel.send({ content: `${Message}` })
+    }
+}
